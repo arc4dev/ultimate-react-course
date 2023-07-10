938833703080f77c8ed0d83a685d6bc0a9ed7096
arc4dev <sroczyk.arkadiusz@gmail.com>
2023-07-10 15:50:33 +0200

    section 10 done

---
+++ 07-usepopcorn/react-use-popcorn/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import StarRating from './components/StarRating';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/* <StarRating defaultRating={5} size={24} color="green" />
+    <StarRating
+      maxRating={5}
+      messages={['terrbile', 'bad', 'okay', 'good', 'amazing']}
+    /> */}
+  </React.StrictMode>
+);
